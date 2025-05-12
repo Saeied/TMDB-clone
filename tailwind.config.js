8b64725500e1dea345a84992382474cf8930432f
saeed <saeied78kh@gmail.com>
2025-05-12 09:25:36 +0330

    added landing page header

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,11 @@ module.exports = {
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#032541",
+      },
+    },
   },
   darkMode: "class",
   plugins: [heroui()],
