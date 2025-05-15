6914202e3931ca4de4fef548b76ce2042d83f162
saeed <saeied78kh@gmail.com>
2025-05-15 19:25:15 +0330

    added movie reviews

--- src/components/movieDetails/MainContent/LeftColumn/index.tsx
+++ src/components/movieDetails/MainContent/LeftColumn/index.tsx
@@ -1,10 +1,12 @@
 import TopBilledCast from "./Cast";
+import Social from "./Social";
 
 export default function LeftColumn() {
   return (
-    <div className="w-full lg:w-[77%]">
+    <div className="w-full lg:w-[77%] flex flex-col">
       <TopBilledCast />
-      <hr className="my-3"/>
+      <hr className="my-3" />
+      <Social />
     </div>
   );
 }
