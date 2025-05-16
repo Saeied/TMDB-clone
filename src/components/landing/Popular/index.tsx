0a82376f833ea629da46006d100390fdb6e55cd2
saeed <saeied78kh@gmail.com>
2025-05-16 12:03:34 +0330

    updated ui

--- src/components/landing/Popular/index.tsx
+++ src/components/landing/Popular/index.tsx
@@ -2,7 +2,6 @@
 import ErrorComponent from "@/components/common/ErrorComponent";
 import Slider from "@/components/common/Slider";
 import instance from "@/services/interceptor";
-import { Button } from "@heroui/button";
 import { useQuery } from "@tanstack/react-query";
 
 function Popular() {
@@ -22,7 +21,6 @@ function Popular() {
     <div className="flex flex-col gap-1 py-10 px-7 lg:px-20 xl:px-52">
       <div className="flex flex-col sm:flex-row gap-4 items-center">
         <h2 className="text-[24px] font-[500]">What's Popular</h2>
-        <Button className="bg-main text-[#1ed5a9]">click</Button>
       </div>
       <div className="relative">
         <Slider isFetching={isFetching} data={data} />
